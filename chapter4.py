e452b0a4af5f1eb81a4cbc79865ed41e60fabdfa
satsukimercado-uf <satsukimercado@ufl.edu>
2025-08-28 21:13:01 -0400

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -8,11 +8,11 @@ def missing_data():
 # example
 import pandas as pd
 import numpy as np
-df = pd.DataFrame [[1.0, 5.0,10.0]
+df = pd.DataFrame ([1.0, 5.0,10.0]
       , [2.0, 6.0, 11.0]
       , [3.0, np.nan, 12.0]
       , [4.0, 8.0, np.nan]
-     ]
+                  )
 df.columns = ['A', 'B', 'C', 'D']
 print(df)
 
