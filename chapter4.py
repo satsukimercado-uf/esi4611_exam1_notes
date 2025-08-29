eb14b503fb593d64b729f83acbd2fd734e8b8c8a
satsukimercado-uf <satsukimercado@ufl.edu>
2025-08-28 21:03:50 -0400

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -7,10 +7,11 @@ def missing_data():
 
 # example
 import pandas as pd
+import numpy as np
 df = [['A', 1.0, 5.0,10.0]
       , ['B', 2.0, 6.0, 11.0]
-      , ['C', 3,0,,12.0]
-      , ['D', 4.0, 8.0, ]
+      , ['C', 3,0, np.nan, 12.0]
+      , ['D', 4.0, 8.0, np.nan]
      ]
 print(df)
 
